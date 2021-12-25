5c336aaaccac8a000d3b9e031ed994bacf06f3ab
viktor286 <just.viktor@gmail.com>
2021-12-25 17:38:03 -0500

    add git icon

--- src/components/CodingAreaHeader/index.js
+++ src/components/CodingAreaHeader/index.js
@@ -1,20 +1,19 @@
 import {useEffect, useState} from 'react';
 import {getTopSvgIcon, defaultTopSvgIcon} from "./topIconIndex";
+import gitHubIcon from './github-brands.svg'
 import "./index.css";
 
 // import {useSelector, useDispatch} from "react-redux";
-// import {signInGithubWithPopup} from "../../modules/githubAuth";
+// import {signInGithubWithPopup} from "../../modules/firebase/githubAuth";
 // import {setUser} from "../../model/redux/auth";
 
 // function TempLogInButton({user}) {
 //   const dispatch = useDispatch();
-//
 //   if (user === 'unknown') {
 //     return <button onClick={async () => {
 //       const authData = await signInGithubWithPopup();
 //       const {displayName} = authData;
 //       dispatch(setUser(displayName));
-//       // TODO: setup auth persistence from page to page & page reload
 //     }}>Login via github
 //     </button>;
 //   } else {
@@ -25,7 +24,7 @@ import "./index.css";
 export default function CodingAreaHeader({currentCodeSample, children}) {
   const [icon, setIcon] = useState(defaultTopSvgIcon);
   const [isHiding, setIsHiding] = useState(true);
-
+  // const {user} = useSelector(state => state.auth);
   useEffect(() => {
     getTopSvgIcon().then(svg => {
       if (svg.startsWith('<svg')) {
@@ -35,7 +34,6 @@ export default function CodingAreaHeader({currentCodeSample, children}) {
     })
   }, []);
 
-  // const {user} = useSelector(state => state.auth);
   const u = new URL(currentCodeSample.html_url);
   return <section className={"codingAreaHeader"}>
     {/*<TempLogInButton user={user} />*/}
@@ -43,7 +41,7 @@ export default function CodingAreaHeader({currentCodeSample, children}) {
     <h2>
       {u.pathname}
       <div className="mainCategory">
-        <a href={u.href}>{u.href}</a>
+        <a href={u.href}><img src={gitHubIcon} alt="code resource on the github.com" title="the origin of this code resource at github.com"/>{u.pathname}</a><sup className="copyright" title={`the license type and the origin of this resource could be found at ${u.href}`}>©</sup>
       </div>
     </h2>
     {children}
