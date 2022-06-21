f36dd509818eec45d350e68b0f7f15cb9231ae4a
viktor286 <just.viktor@gmail.com>
2022-06-21 12:26:01 -0400

    adj small things

---
+++ src/modules/persistence/firebase/firebaseConfig.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDdW1p5bIqO-Mix52R9j5HSYZxvHpeu4mE',
+  authDomain: 'retypeproject.com',
+  databaseURL: 'https://typer-trainer.firebaseio.com',
+  projectId: 'typer-trainer',
+  storageBucket: 'typer-trainer.appspot.com',
+  appId: '1:430784604149:web:919faceafdea3c609c22bc',
+};
