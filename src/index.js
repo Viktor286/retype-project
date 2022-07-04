71376ed3e2675c148687e4bb9409cbbbac20e4d8
Alex Navasardyan <alex.navasardyan@gmail.com>
2022-07-04 09:46:01 -0400

    Re-run prettier on the codebase

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {Provider} from "react-redux";
-import initStore from "./model/redux";
-import CodeTrainerApp from "./components/CodeTrainerApp/CodeTrainerApp";
-import "./globals.css";
-import {prepareAuth} from "./modules/persistence/firebase";
-import {setUser} from "./model/redux/auth";
-import {initializeUserByAuthData} from "./modules/persistence";
-import LandingPage from "./components/LandingPage";
-import ModalWindow from "./components/ModalWindow";
+import { Provider } from 'react-redux';
+import initStore from './model/redux';
+import CodeTrainerApp from './components/CodeTrainerApp/CodeTrainerApp';
+import './globals.css';
+import { prepareAuth } from './modules/persistence/firebase';
+import { setUser } from './model/redux/auth';
+import { initializeUserByAuthData } from './modules/persistence';
+import LandingPage from './components/LandingPage';
+import ModalWindow from './components/ModalWindow';
 // import reportWebVitals from './reportWebVitals';
 
 window.debugLogConfig = {
-  "redux-log": 0,
+  'redux-log': 0,
   changeCodeSample: 1,
   codeSampleComplete: 1,
-  saveCodeSamplesPlaylistToLS: 1
+  saveCodeSamplesPlaylistToLS: 1,
 };
 
 window.codeTrainerApp = {
@@ -27,7 +27,7 @@ const store = initStore();
 
 async function entryPoint() {
   if (!store.getState()?.auth?.config) {
-    const {authData: githubAuthData, auth} = await prepareAuth(store);
+    const { authData: githubAuthData, auth } = await prepareAuth(store);
     window.codeTrainerApp.auth = auth;
     const userJourneyData = await initializeUserByAuthData(githubAuthData);
     store.dispatch(setUser(userJourneyData));
@@ -37,10 +37,10 @@ async function entryPoint() {
   root.render(
     <>
       <Provider store={store}>
-        <ModalWindow/>
-        {window.location.pathname === '/' ? <LandingPage/> : <CodeTrainerApp/>}
+        <ModalWindow />
+        {window.location.pathname === '/' ? <LandingPage /> : <CodeTrainerApp />}
       </Provider>
-    </>
+    </>,
   );
 }
 
@@ -52,9 +52,7 @@ entryPoint();
 // reportWebVitals(console.log);
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker
-    .register('/sw.js')
-    .then(() => {
-      // console.log('Service Worker Registered');
-    });
+  navigator.serviceWorker.register('/sw.js').then(() => {
+    // console.log('Service Worker Registered');
+  });
 }
