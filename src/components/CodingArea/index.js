659687e4295150cff7648df0492e2c4c23b0f053
viktor286 <just.viktor@gmail.com>
2022-07-24 11:30:56 -0400

    add font-controls experimental feature

--- src/components/CodingArea/index.js
+++ src/components/CodingArea/index.js
@@ -1,5 +1,5 @@
 import './index.css';
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { toggleTypingMode } from '../../model/redux/stats';
 
@@ -12,9 +12,33 @@ const CodingArea = ({
   const stats = useSelector((state) => state.stats);
   const { typingMode } = stats;
 
+  const [font, setFont] = useState(4);
+  const fontSizes = [
+    'fs-xxxs',
+    'fs-xxs',
+    'fs-xs',
+    'fs-s',
+    'fs-m',
+    'fs-l',
+    'fs-xl',
+    'fs-xxl',
+    'fs-xxxl',
+    'fs-4xl',
+  ];
+  const onFontDecrease = () => {
+    setFont(font + -1 < 0 ? font : font - 1);
+  };
+  const onFontIncrease = () => {
+    setFont(font + 1 > fontSizes.length - 1 ? font : font + 1);
+  };
+
   return (
     <>
-      <section className={`codingArea ${typingMode === 0 ? 'fade-out-typing-mode' : 'fade-in-typing-mode'}`}>
+      <section
+        className={`codingArea ${fontSizes[font]} ${
+          typingMode === 0 ? 'fade-out-typing-mode' : 'fade-in-typing-mode'
+        }`}
+      >
         <div className="top-bar">
           {/* temporary make as a button to keep away from keyboard enter functionality */}
           <div
@@ -26,6 +50,15 @@ const CodingArea = ({
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
               <path d="M512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0C397.4 0 512 114.6 512 256zM256 64V448C362 448 448 362 448 256C448 149.1 362 64 256 64z" />
             </svg>
+          </div>
+          {/* temporary quick-make font-controls here as experimental feature */}
+          <div className="font-controls">
+            <button className="font-decrease" onClick={onFontDecrease}>
+              A-
+            </button>
+            <button className="font-increase" onClick={onFontIncrease}>
+              A+
+            </button>
           </div>
         </div>
         {contentAs2dArray.map((linesArr, lineNumber) => {
