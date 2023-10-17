2d524ae60c7f9c28e21e1c3d08f4bc373b43b4f6
viktor286 <just.viktor@gmail.com>
2023-10-16 21:11:54 -0400

    start new landing

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,18 +1,15 @@
-import "./index.css";
-import { useEffect } from "react";
+import './index.css';
+import { useEffect } from 'react';
 
 async function loadFrontPageContent() {
-  const res = await fetch("/data/showcase/js-01.json");
+  const res = await fetch('/data/showcase/js-01.json');
   const data = await res.json();
-  console.log("@@ data", data);
+  console.log('@@ data', data);
 }
 
 export default function LandingPage() {
   useEffect(() => {
     loadFrontPageContent();
   }, []);
-  return (<>New landing
-    </>
-  )
-    ;
+  return <>New landing</>;
 }
